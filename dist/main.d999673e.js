acf8d93e3b2e979f35745f2edc7080005731c6ca
injashkin <injashkin@gmail.com>
2020-06-18 00:57:54 +0300

    Почти закончил компонент CHECKBOX
    Осталась одна проблема - послений чекбокс сжат по горизонтали, как
    будто его сдавливает текст.

--- dist/main.d999673e.js
+++ dist/main.d999673e.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./image.jpg":[["image.faa22318.jpg","../theme/image.jpg"],"../theme/image.jpg"],"_css_loader":"../../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../components/checkbox/vector.svg":[["vector.a97c5277.svg","../components/checkbox/vector.svg"],"../components/checkbox/vector.svg"],"./image.jpg":[["image.faa22318.jpg","../theme/image.jpg"],"../theme/image.jpg"],"_css_loader":"../../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43623" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42979" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
