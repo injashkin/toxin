c72c15b07e652551c72cc0b8eebed3b09fb52efb
injashkin <injashkin@gmail.com>
2020-06-23 00:54:07 +0300

    Применил миксины к TEXT FIELD
    Теперь один компонент TEXT FIELD может создавать два поля:
    TEXT FIELD и SUBSCRIPTION TEXT FIELD

--- dist/ui-kit/ui-kit.js
+++ dist/ui-kit/ui-kit.js
@@ -184,12 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../theme/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"./fonts/montserrat-v14-latin_cyrillic-regular.eot":[["montserrat-v14-latin_cyrillic-regular.9c413f0d.eot","../theme/fonts/montserrat-v14-latin_cyrillic-regular.eot"],"../theme/fonts/montserrat-v14-latin_cyrillic-regular.eot"],"./fonts/montserrat-v14-latin_cyrillic-regular.woff":[["montserrat-v14-latin_cyrillic-regular.2ad30f1c.woff","../theme/fonts/montserrat-v14-latin_cyrillic-regular.woff"],"../theme/fonts/montserrat-v14-latin_cyrillic-regular.woff"],"./fonts/montserrat-v14-latin_cyrillic-regular.ttf":[["montserrat-v14-latin_cyrillic-regular.62472a27.ttf","../theme/fonts/montserrat-v14-latin_cyrillic-regular.ttf"],"../theme/fonts/montserrat-v14-latin_cyrillic-regular.ttf"],"./fonts/montserrat-v14-latin_cyrillic-regular.svg":[["montserrat-v14-latin_cyrillic-regular.c4759a18.svg","../theme/fonts/montserrat-v14-latin_cyrillic-regular.svg"],"../theme/fonts/montserrat-v14-latin_cyrillic-regular.svg"],"./fonts/montserrat-v14-latin_cyrillic-700.eot":[["montserrat-v14-latin_cyrillic-700.69095323.eot","../theme/fonts/montserrat-v14-latin_cyrillic-700.eot"],"../theme/fonts/montserrat-v14-latin_cyrillic-700.eot"],"./fonts/montserrat-v14-latin_cyrillic-700.woff":[["montserrat-v14-latin_cyrillic-700.828a8ed1.woff","../theme/fonts/montserrat-v14-latin_cyrillic-700.woff"],"../theme/fonts/montserrat-v14-latin_cyrillic-700.woff"],"./fonts/montserrat-v14-latin_cyrillic-700.ttf":[["montserrat-v14-latin_cyrillic-700.ae6647a6.ttf","../theme/fonts/montserrat-v14-latin_cyrillic-700.ttf"],"../theme/fonts/montserrat-v14-latin_cyrillic-700.ttf"],"./fonts/montserrat-v14-latin_cyrillic-700.svg":[["montserrat-v14-latin_cyrillic-700.38d4f879.svg","../theme/fonts/montserrat-v14-latin_cyrillic-700.svg"],"../theme/fonts/montserrat-v14-latin_cyrillic-700.svg"],"./../components/checkbox/vector.svg":[["vector.a97c5277.svg","../components/checkbox/vector.svg"],"../components/checkbox/vector.svg"],"./image.jpg":[["image.faa22318.jpg","../theme/image.jpg"],"../theme/image.jpg"],"_css_loader":"../../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35311" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42265" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +389,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/main.d999673e.js.map
+//# sourceMappingURL=/ui-kit/ui-kit.js.map
