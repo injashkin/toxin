dd3a08ded18758ca2ae90db68648a98d12bd0f19
injashkin <injashkin@gmail.com>
2020-06-27 22:07:27 +0300

    Переделал кнопку-компонент CLICK ME
    Пришлось ее переделывать, так как в html файле она была ссылкой, а
    не батоном. Решил применить эту кнопку в футере дропдауна. В
    дальнейшем, наверное все кнопки объединю в один компонент, и в
    нужном месте буду изменять их с помощью модификаторов.

--- dist/dropdown.6fe1a231.js
+++ dist/dropdown.6fe1a231.js
@@ -11369,7 +11369,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41461" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39617" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ src/components/button4/button4.js
@@ -0,0 +1,1 @@
+import "./button4.scss";
