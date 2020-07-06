19849eb552080f3f22cc4f911c18b25c7095fcb3
injashkin <injashkin@gmail.com>
2020-07-06 17:05:56 +0300

    Закончен компонент RATE BUTTON
    Потратил много времени на поиск ошибки, которая заключалась в том,
    что в js коде я написал star__border (два подчеркивания) вместо
    star_border (одно подчеркивание).

--- dist/rate.100c2d0b.js
+++ dist/rate.100c2d0b.js
@@ -196,54 +196,38 @@ require("./rate.scss");
 
 $(function () {
   $(".rating__checkbox").on("click", function () {
-    var elem = $(this).parent().parent().find(".rating__checkbox");
-    var stars = $(this).parent().parent().find(".rating__star"); //удаление атрибута checked и обнуление звезд
+    var elem = $(this).parent().parent().find(".rating__checkbox"); //Удаление атрибута checked
 
     for (var i = 0; i < elem.length; i++) {
       $(elem[i]).attr("checked", false);
-      $(stars[i]).text();
     }
 
-    var check = [0, 0, 0, 0, 0];
-    var star = [0, 0, 0, 0, 0]; //Установка атрибута checked нажатому элементу
+    var star = []; //Установка атрибута checked нажатому элементу
 
     $(this).attr("checked", true);
-    console.log($(this).attr("checked", true));
-    elem = $(this).parent().parent().find(".rating__checkbox");
 
     for (var i = 0; i < elem.length; i++) {
       if ($(elem[i]).attr("checked")) {
-        console.log($(elem[i]).attr("checked"));
-        check[i] = 1;
         star[i] = 1;
-      }
-    } //var elem = $(this).parent().parent().find(".rating__checkbox");
-    //var star = $(this).parent().parent().find(".rating__star");
-    //Поиск элемента с атрибутом checked
-
-
-    var trigger = 0;
-    var j = check.length - 1;
-    console.log("--------------------------------------------");
-
-    for (j; j >= 0; j--) {
-      if (check[j] == 1) {
-        //$(elem[j]).attr("checked")
-        trigger = 1;
-      }
-
-      if (trigger == 1) {//На нажатом и всех предшествующих ему элементах загорается звезда
-        //$(this).parent().children(".rating__star").text("star");
-        //$(star[j]).text("star");
       } else {
-        //выключаем звезду
-        //$(star[j]).text("star__border");
-        star[j] = 0; //console.log("trigger " + trigger);
+        star[i] = 0;
       }
+    } //Зажигание всех звезд от элемента с атрибутом checked до начала
 
-      console.log("j " + j);
-      console.log(star);
+
+    for (var i = elem.length; i >= 0; i--) {
+      if (star[i] == 1 && i != 0) {
+        star[i - 1] = 1;
+      }
     }
+
+    $(this).parent().parent().find(".rating__star").each(function (index) {
+      if (star[index] == 1) {
+        $(this).text("star");
+      } else {
+        $(this).text("star_border");
+      }
+    });
   });
 }); //console.log();
 },{"./rate.scss":"../components/rate/rate.scss"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -274,7 +258,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38695" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37575" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/components/rate/rate.js
+++ src/components/rate/rate.js
@@ -3,56 +3,43 @@ import "./rate.scss";
 $(function () {
   $(".rating__checkbox").on("click", function () {
     var elem = $(this).parent().parent().find(".rating__checkbox");
-    var stars = $(this).parent().parent().find(".rating__star");
 
-    //удаление атрибута checked и обнуление звезд
+    //Удаление атрибута checked
     for (var i = 0; i < elem.length; i++) {
       $(elem[i]).attr("checked", false);
-      $(stars[i]).text();
     }
 
-    var check = [0, 0, 0, 0, 0];
-    var star = [0, 0, 0, 0, 0];
+    var star = [];
 
     //Установка атрибута checked нажатому элементу
     $(this).attr("checked", true);
 
-    elem = $(this).parent().parent().find(".rating__checkbox");
-
     for (var i = 0; i < elem.length; i++) {
       if ($(elem[i]).attr("checked")) {
-        console.log($(elem[i]).attr("checked"));
-        check[i] = 1;
         star[i] = 1;
-      }
-    }
-
-    //var elem = $(this).parent().parent().find(".rating__checkbox");
-    //var star = $(this).parent().parent().find(".rating__star");
-
-    //Поиск элемента с атрибутом checked
-    var trigger = 0;
-    var j = check.length - 1;
-    console.log("--------------------------------------------");
-    for (j; j >= 0; j--) {
-      if (check[j] == 1) {
-        //$(elem[j]).attr("checked")
-        trigger = 1;
-      }
-
-      if (trigger == 1) {
-        //На нажатом и всех предшествующих ему элементах загорается звезда
-        //$(this).parent().children(".rating__star").text("star");
-        //$(star[j]).text("star");
       } else {
-        //выключаем звезду
-        //$(star[j]).text("star__border");
-        star[j] = 0;
-        //console.log("trigger " + trigger);
+        star[i] = 0;
       }
-      console.log("j " + j);
-      console.log(star);
     }
+
+    //Зажигание всех звезд от элемента с атрибутом checked до начала
+    for (var i = elem.length; i >= 0; i--) {
+      if (star[i] == 1 && i != 0) {
+        star[i - 1] = 1;
+      }
+    }
+
+    $(this)
+      .parent()
+      .parent()
+      .find(".rating__star")
+      .each(function (index) {
+        if (star[index] == 1) {
+          $(this).text("star");
+        } else {
+          $(this).text("star_border");
+        }
+      });
   });
 });
 
