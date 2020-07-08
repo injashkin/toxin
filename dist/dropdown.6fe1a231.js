1616dad53a6f410d4730c7c48a67efa157f86d98
injashkin <injashkin@gmail.com>
2020-07-08 13:06:26 +0300

    Впервые применил атрибуты в BUTTON
    Сегодня возникла необходимость передать классы из компонента
    DROPDOWN в компонент BUTTON, и в этом мне помогли атрибуты.

--- dist/dropdown.6fe1a231.js
+++ dist/dropdown.6fe1a231.js
@@ -11372,7 +11372,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42217" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42077" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
