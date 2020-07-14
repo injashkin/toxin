f1ca0852ed3a0dee4584a5bc7fd02bf2c1c0ed49
injashkin <injashkin@gmail.com>
2020-07-15 01:00:19 +0300

    Создал INPUT и форму SIGNIN, доработал BUTTON.

--- dist/dropdown.6fe1a231.js
+++ dist/dropdown.6fe1a231.js
@@ -11489,7 +11489,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38669" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33129" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
