a13b608c7683778d00cc724d7a7eced8063fe098
injashkin <injashkin@gmail.com>
2020-07-16 16:55:07 +0300

    Закончил форму SEARCH ROOMS

--- dist/color-item.e6754a18.js
+++ dist/color-item.e6754a18.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45367" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43601" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
