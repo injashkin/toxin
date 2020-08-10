7bf6228636228bba14939273d6cac1e59b46065a
injashkin <injashkin@gmail.com>
2020-08-11 02:17:42 +0300

    Доработаны Header и Menu для mobile

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,1 +1,18 @@
 import "./menu.scss";
+var $ = require("jquery");
+
+$(function () {
+  $(".menu-mobile__icon").click(function () {
+    $(this).toggleClass("menu-mobile__icon_active");
+    if ($(this).hasClass("menu-mobile__icon_active")) {
+      $(this).children(".material-icons").text("close");
+      $(this).parent().children(".menu-mobile").addClass("menu-mobile_active");
+    } else {
+      $(this).children(".material-icons").text("menu");
+      $(this)
+        .parent()
+        .children(".menu-mobile")
+        .removeClass("menu-mobile_active");
+    }
+  });
+});
