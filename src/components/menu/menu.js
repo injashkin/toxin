ad6c46b667033e07eac525d90861813e47e72913
injashkin <injashkin@gmail.com>
2020-08-12 22:46:43 +0300

    Доработан респонсив для Header и Menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,6 +4,7 @@ var $ = require("jquery");
 $(function () {
   $(".menu-mobile__icon").click(function () {
     $(this).toggleClass("menu-mobile__icon_active");
+    $("body").toggleClass("lock");
     if ($(this).hasClass("menu-mobile__icon_active")) {
       $(this).children(".material-icons").text("close");
       $(this).parent().children(".menu-mobile").addClass("menu-mobile_active");
