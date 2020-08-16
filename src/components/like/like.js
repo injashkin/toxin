3ca0ec1eae382f260106764f5bc785a5c1854767
injashkin <injashkin@gmail.com>
2020-08-16 19:06:31 +0300

    Доработан Like

--- src/components/like/like.js
+++ src/components/like/like.js
@@ -1,15 +1,17 @@
 import "./like.scss";
 
 $(function () {
-  $(".like-button").click(function () {
+  const $likeButton = $(".js-like-button");
+  $likeButton.on("click.likeButton", function () {
+    const $buttonClicked = $(this);
     // Изменяем внешний вид кнопки
-    $(this).toggleClass("like-button__active");
-    const classActive = $(this).hasClass("like-button__active");
+    $buttonClicked.toggleClass("js-like-button__active");
+    const classActive = $buttonClicked.hasClass("js-like-button__active");
     let icon = classActive ? "favorite" : "favorite_border";
-    $(this).find("i").text(icon);
+    $buttonClicked.find("i").text(icon);
     // Инкремент/декремент количества лайков
-    let count = $(this).find(".like-button__number").text();
+    let count = $buttonClicked.find(".like-button__number").text();
     count = classActive ? ++count : --count;
-    $(this).find(".like-button__number").text(count);
+    $buttonClicked.find(".like-button__number").text(count);
   });
 });
