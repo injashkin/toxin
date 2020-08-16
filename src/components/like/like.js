3c0c2b2e37380830a6cc530fe0b3ff82db322d16
injashkin <injashkin@gmail.com>
2020-08-16 15:49:46 +0300

    Доработан Like и вставлен в Room

--- src/components/like/like.js
+++ src/components/like/like.js
@@ -2,6 +2,14 @@ import "./like.scss";
 
 $(function () {
   $(".like-button").click(function () {
+    // Изменяем внешний вид кнопки
     $(this).toggleClass("like-button__active");
+    const classActive = $(this).hasClass("like-button__active");
+    let icon = classActive ? "favorite" : "favorite_border";
+    $(this).find("i").text(icon);
+    // Инкремент/декремент количества лайков
+    let count = $(this).find(".like-button__number").text();
+    count = classActive ? ++count : --count;
+    $(this).find(".like-button__number").text(count);
   });
 });
