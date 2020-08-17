83f46e045046528d5ebe532a3c919872a0039ed6
injashkin <injashkin@gmail.com>
2020-08-17 14:18:26 +0300

    Удален компонент Text-field
    Его полностью заменит Input

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -229,5 +229,10 @@ module.exports = {
       filename: "./room.html",
       inject: true,
     }),
+    new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/pages/cards/cards.pug`,
+      filename: "./cards.html",
+      inject: true,
+    }),
   ],
 };
