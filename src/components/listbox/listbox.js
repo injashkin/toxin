b721ed4aabc8584b28247e2865fb409cb1e05fe6
injashkin <injashkin@gmail.com>
2020-08-18 22:48:47 +0300

    Изменен Listbox
    Написан JQ скрипт, для обработки клика на заголовке списка. При
    клике открывается/закрывается список

--- src/components/listbox/listbox.js
+++ src/components/listbox/listbox.js
@@ -1,1 +1,12 @@
 import "./listbox.scss";
+
+$(function () {
+  let $listboxHeader = $(".js-listbox__header");
+  $listboxHeader.on("click", function () {
+    $(this).find(".js-listbox__arrow").toggleClass("js-listbox__arrow_rotate");
+    $(this)
+      .parent()
+      .find(".js-listbox__drop")
+      .toggleClass("js-listbox__drop_close");
+  });
+});
